f04d9b76800b1c6a8daf36ed1d2a0b1702bdda0e
jjub0217 <noonssub1004@gmail.com>
2020-06-03 10:25:12 +0900

    modify my study map.js

--- map2.js
+++ map2.js
@@ -13,9 +13,9 @@ const items = [{
 // 화살표 함수를 할당할 새로운 변수를 만들고, (혹은 화살표 함수를 담을 함수를 작성)그 함수를 배열의 요소의 값의 의미를 나타내는 단어를 써서,
 // 그 단어를 매개변수로 받아오고, 그 해당 단어를 사용하여 함수 몸체를 작성한다.
 // map 고차함수를 쓰면서 사용되는 괄호 안에는 위에 화살표 함수를 할당한 변수를 담아줘야 한다.
-// const text = item => item.text;
-// const texts = items.map(text);
-// console.log(texts);
+const text = item => item.text;
+const texts = items.map(text);
+console.log(texts);
 
 // [ 'hello', 'bye' ]
 
