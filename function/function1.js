5df8ca543e53933a60f44383da150be4cbfd7e51
jjub0217 <noonssub1004@gmail.com>
2020-06-28 19:02:12 +0900

    dev: javascript_function

--- function/function1.js
+++ function/function1.js
@@ -1,15 +1,13 @@
-function multiple(a, b = 5, c = 10) {
-    return a * b + c;
-}
+// function addNumber(a, b) { // <- 이 줄에서 말하는 a 와 b 는 함수를 실행할때 "필요하다고" "미리" "지정"한 값이다. 
+//     let sum = a + b;
+//     console.log(sum);
+// }
 
-// multiple(5, 10, 20); // 70
-console.log(multiple(5, 10, 20)); // 70
-// a = 5 , b = 10, c = 20
+let num1 = Number(prompt('첫번째 숫자는?'));
+let num2 = Number(prompt('두번째 숫자는?'));
+addNumber(num1, num2);
 
-// multiple(10, 20); // 210
-console.log(multiple(10, 20));  // 210
-// a = 10, b = 20, c = 10 (c = 10 은 매개변수 위치에 미리 지정해둔 기본값)
-
-// multiple(30); // 160
-console.log(multiple(30));  // 160
-// a = 30, b = 5, c = 10 (b = 5 와 c = 10 은 매개변수 위치에 미리 지정해둔 기본값)
+function addNumber(a, b){
+let sum = a + b;
+alert(`두수를 더한 값은 ${sum}입니다.`);
+} 
